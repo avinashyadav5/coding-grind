0689b4a165a482a9a385d29208d7d70b464ecac9
avinashyadav5 <aviydv7518@gmail.com>
2025-05-25 00:16:44 +0530

    Time: 5 ms (26.98%), Space: 41.9 MB (24.26%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String s="";
+        for(int i=0;i<address.length();i++)
+        {
+            char ch=address.charAt(i);
+            if(ch=='.')
+            {
+                s+="[.]";
+            }
+            else
+            s+=ch;
+        }
+        return s;
+    }
+}
